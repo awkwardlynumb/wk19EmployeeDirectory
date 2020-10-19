df1413803ded82a8afb558d031787a40e555b0f0
Matt Pearce <pearce.matthewa@gmail.com>
2020-10-19 16:37:42 -0600

    update before you grade I hope

--- employeedirectory/src/components/table.js
+++ employeedirectory/src/components/table.js
@@ -9,6 +9,7 @@ import React from "react"
 
 
 function Table(props) {
+    console.log(props.employees)
     return(
     <table class="table">
         <thead>
@@ -16,16 +17,16 @@ function Table(props) {
                 <th scope="col">Name</th>
                 <th scope="col">Email</th>
                 <th scope="col">Phone</th>
-                <th scope="col">DOB</th>
+                <th scope="col">Location</th>
             </tr>
         </thead>
         <tbody>
         {props.employees.map(employee => (
           <tr key={employee.id.value}>
               <td>{employee.name.first} {employee.name.last}</td>
-              <td>{employee.name.first} {employee.name.last}</td>
-              <td>{employee.name.first} {employee.name.last}</td>
-              <td>{employee.name.first} {employee.name.last}</td>
+              <td>{employee.email}</td>
+              <td>{employee.phone}</td>
+              <td>{employee.location.city}</td>
           </tr>
         ))}
         </tbody>
